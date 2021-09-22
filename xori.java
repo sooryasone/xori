7f71d993648e4123294196142e04808c5d00c493
sooryasone <sooryasone@gmail.com>
2021-09-22 05:39:26 +0000

    java test

---
+++ xori.java
@@ -0,0 +1,10 @@
+public class xori{
+
+    public static void main(String[] args){
+
+   	for(int i=1;i<=3;i++) {
+       System.out.println("Hello Xorainters! Welcome to the test session"+i);
+}
+      
+    }
+}
