61b102ce387ebfd094f5857210161c58c0f4cfbb
sooryasone <sooryasone@gmail.com>
2021-09-22 05:59:06 +0000

    changed again

--- xori.java
+++ xori.java
@@ -2,7 +2,7 @@ public class xori{
 
     public static void main(String[] args){
 
-   	for(int i=1;i<=3;i++) {
+   	for(int i=1;i<=5;i++) {i
        System.out.println("Hello Xorainters! Welcome to the training  session"+i);
 }
       
